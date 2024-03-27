39b54e468bc5f8c820333aadab2ad725bb940d3c
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-03-27 06:44:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,4 @@ API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
-
-# for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-

a1bb8f70661c50dd94f64eeb45849a30b2559f3b
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-03-27 06:55:55 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -13,7 +13,7 @@ async def rename_file(bot, msg):
        await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
     og_media = getattr(reply, reply.media.value)
     new_name = msg.text.split(" ", 1)[1]
-    sts = await msg.reply_text("Trying to Downloading.....")
+    sts = await msg.reply_text("Trying to Download.....")
     c_time = time.time()
     downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
     filesize = humanbytes(og_media.file_size)                
@@ -24,8 +24,7 @@ async def rename_file(bot, msg):
             return await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")           
     else:
         cap = f"{new_name}\n\n💽 size : {filesize}"
-
-    # this idea's back end is MKN brain 🧠
+        
 
     dir = os.listdir(DOWNLOAD_LOCATION)
     if len(dir) == 0:
@@ -38,10 +37,10 @@ async def rename_file(bot, msg):
             print(e)        
             og_thumbnail = None
         
-    await sts.edit("Trying to Uploading")
+    await sts.edit("Trying to Upload")
     c_time = time.time()
     try:
-        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploading Started.....", sts, c_time))        
     except Exception as e:  
         return await sts.edit(f"Error {e}")                       
     try:

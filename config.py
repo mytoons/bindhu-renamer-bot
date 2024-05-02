0bf0d44f2b4fdc7efe7c27e62381b61a279a3a12
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-05-02 15:34:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "4857766"))
 API_HASH = environ.get("API_HASH", "6c3c6facf5598a4b318e138f8c407028")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5950478723:AAELUayqnKQdVZoio9PuVXskEnfdfXInpOg")
 ADMIN = int(environ.get("ADMIN", "1596559467"))          
-CAPTION = environ.get("CAPTION", "")
+CAPTION = environ.get("CAPTION", "{file_name} Bot Powered By @maheshsirop")
 DOWNLOAD_LOCATION = "./DOWNLOADS"

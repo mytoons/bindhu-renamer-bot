dea57c70617a0051fe7864c1de9f548b2ce47959
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-05-02 15:27:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "4857766"))
+API_HASH = environ.get("API_HASH", "6c3c6facf5598a4b318e138f8c407028")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+ADMIN = int(environ.get("ADMIN", "1596559467"))          
 CAPTION = environ.get("CAPTION", "")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
